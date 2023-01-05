eacf1f3b89a0d11dc5af1f714c1b57fa72afc9fb
yasseen <x@x>
2023-01-05 22:20:40 +0300

    1:06:53

--- lib/gamefield.dart
+++ lib/gamefield.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,14 +10,23 @@ class GameField extends StatefulWidget {
   State<GameField> createState() => _GameFieldState();
 }
 
+enum Direction{up,down,left,right}
+
 class _GameFieldState extends State<GameField> {
+  int lenght = 4;
 
-
+  Direction _direction = Direction.up;
   List<Offset> _positions=[];
   double? height;
   double? widht;
+
   @override
   Widget build(BuildContext context) {
+    Timer.periodic(Duration(milliseconds: 500), (timer) {
+      setState(() {
+
+      });
+    });
     height=MediaQuery.of(context).size.height;
     widht=MediaQuery.of(context).size.width;
     return Scaffold(
@@ -33,7 +43,7 @@ class _GameFieldState extends State<GameField> {
    });
    for (int i=0; i< _positions.length; i++){
      pieces.add(
-       Piece(size: 15, posx: _positions[i].dx, posy: _positions[i].dy, color: Colors.lightBlueAccent,)
+       Piece(size: 15, posx: _positions[i].dx, posy: _positions[i].dy, color: Colors.redAccent,)
      );
    }
    return pieces;
@@ -43,7 +53,32 @@ class _GameFieldState extends State<GameField> {
     if(_positions.length==0){
       _positions.add(getRamdomPositionWithSreen());
     }
+    for (int i = _positions.length-1;i>0;i--){
+      _positions[i]=_positions[i-1];
+    }
+    _positions[0]=checkBorder(_positions[0]);
+    _positions[0] = getNextPostion(_positions[0]);
   }
+
+  //TODO Next position
+  Offset getNextPostion(Offset position){
+    Offset nextPosition = Offset(5, 6);
+
+    if (_direction==Direction.up)nextPosition = position - Offset(0, 5);
+    if (_direction==Direction.down)nextPosition = position + Offset(0, 5);
+    if (_direction==Direction.right)nextPosition = position + Offset(5, 0);
+    if (_direction==Direction.left)nextPosition = position - Offset(5, 0);
+checkBorder(position);
+    return nextPosition;
+  }
+
+checkBorder(Offset position){
+    if(position.dy>=height!-30) position=Offset(position.dx, 1);
+      if(position.dy<=0) position=Offset(position.dx, height!-1);
+    if(position.dx>=widht!-30) position=Offset(1, position.dy);
+      if(position.dx<=0) position=Offset(height!-1, position.dy);
+    return position;
+}
 
   Offset getRamdomPositionWithSreen(){
     Random rand = Random();
